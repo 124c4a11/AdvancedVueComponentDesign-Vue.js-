7c3bc3a618ae06aa47b3d1caed446961547265df
124c4a11 <124c4a11@mail.ru>
2019-02-11 11:58:03 +0300

    DatePicker

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">UserSettingsForm</router-link>
+      <router-link to="/">UserSettingsForm</router-link> |
+      <router-link to="/datepicker">Datepicker</router-link>
     </div>
     <router-view/>
   </div>
@@ -13,9 +14,12 @@
 #nav {
   padding: 30px;
   text-align: center;
+
   a {
     font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
+
     &.router-link-exact-active {
       color: #42b983;
     }
