ee34160a9751d916cf3bedc2c2bc75ead2fce3de
124c4a11 <124c4a11@mail.ru>
2019-02-11 11:19:29 +0300

    UserSettingsForm: ToggleInput

---
+++ src/components/ToggleInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <span class="toggle" role="checkbox" tabindex="0"
+    @click='toggle'
+    @keydown.space.prevent='toggle'
+    :aria-checked='toggler.toString()'
+  ></span>
+</template>
+
+<script>
+export default {
+  props: ['toggler'],
+
+  model: {
+    prop: 'toggler',
+    event: 'toggle'
+  },
+
+  methods: {
+    toggle () {
+      this.$emit('toggle', !this.toggler)
+    }
+  }
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import SettingsForm from './views/SettingsForm.vue'
 
 Vue.use(Router)
 
@@ -10,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'settings-form',
+      component: SettingsForm
     }
   ]
 })
