7ce4be4f16495bcb0fd65da5fd0c59e26d46c5ed
124c4a11 <124c4a11@mail.ru>
2019-02-11 10:00:04 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import './assets/css/app.css';
+
+#nav {
+  padding: 30px;
+  text-align: center;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
